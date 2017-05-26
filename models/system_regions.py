ff3806e52b8d2a9e300f0a1a901b4da6f04babb6
carlos torres <snotchstar@gmail.com>
2017-05-26 19:42:56 -0400

    changed get to set for md5

--- models/system_regions.py
+++ models/system_regions.py
@@ -10,26 +10,26 @@ class System_Region():
 		last_updated, region_id, name
 
 		region_md5 is calculated using the hashlib md5
-
-		self.listed is a list in the following order:
-		last_updated, region_id, name, region_md5
-		used to to insert into db
-
-		self.type should be in all models to ensure source/tgt is correct
 		'''
 		self.last_updated = record['last_updated']
 		#convert region_id to int
 		self.region_id = int(record['region_id'])
 		self.name = record['name']
-		self.region_md5 = self.get_region_md5()
+		self.set_region_md5()
 
 
 
 	def __repr__(self):
 		return '<name {}>'.format(self.name)
 
-	def get_region_md5(self):
-		return hashlib.md5(str.encode(str(self.region_id) + self.name)).hexdigest()
+	def set_region_md5(self):
+		self.region_md5 = hashlib.md5(str.encode(str(self.region_id)\
+									 + self.name)).hexdigest()
 
 	def to_list(self):
+		'''
+		returns a list of the attributes in the following order:
+		last_updated, region_id, name, region_md5
+		used to to insert into db
+		'''
 		return [self.last_updated, self.region_id, self.name, self.region_md5]
