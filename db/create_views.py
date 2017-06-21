add9c861f4f252d21cf883ce9fd7c4877546d1e3
carlos torres <snotchstar@gmail.com>
2017-06-21 17:33:35 -0400

    created view for latest station_status

--- db/create_views.py
+++ db/create_views.py
@@ -39,12 +39,36 @@ create_station_information_view = '''CREATE VIEW v_station_information AS
 							 	AND transtype != 'D'
 								'''
 
+create_station_status_view = '''CREATE VIEW v_station_status AS
+								SELECT
+								latest.last_updated, 
+								latest.station_id, 
+								station_status.num_bikes_available, 
+								station_status.num_bikes_disabled, 
+								station_status.num_docks_available, 
+								station_status.num_docks_disabled, 
+								station_status.is_installed, 
+								station_status.is_renting, 
+								station_status.is_returning, 
+								station_status.last_reported
+								FROM (
+									SELECT MAX(last_updated) AS last_updated,station_id
+									FROM station_status
+									GROUP BY station_id) latest
+								INNER JOIN station_status 
+								ON	latest.last_updated = station_status.last_updated
+								AND latest.station_id = station_status.station_id
+
+'''
+
 def main():
 	connection = sqlite3.connect('bikeshare.db')
-	connection.execute('DROP VIEW v_station_information')
-	connection.execute('DROP VIEW v_system_regions')
-	connection.execute(create_system_regions_view)
-	connection.execute(create_station_information_view)
+#	connection.execute('DROP VIEW v_station_information')
+#	connection.execute('DROP VIEW v_system_regions')
+	connection.execute('DROP VIEW v_station_status')
+#	connection.execute(create_system_regions_view)
+#	connection.execute(create_station_information_view)
+	connection.execute(create_station_status_view)
 	connection.commit()
 	connection.close()
 
