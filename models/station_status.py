3ea7119052f55b1fe1d4255da0e9425610e7febe
carlos torres <snotchstar@gmail.com>
2017-06-22 21:31:15 -0400

    cdc in progress

--- models/station_status.py
+++ models/station_status.py
@@ -11,25 +11,47 @@ class Station_Status():
 		num_docks_disabled, is_installed, is_renting,
 		is_returning, last_reported
 
+		record can also be tuple (from sql).
+		if tuple, get/set is different since no keys
+
 		this one is different than others since it doesn't calc md5
 
 		any fields that aren't require use set_[] to prevent KeyError
 		'''
 		self.record = record
 
-		self.last_updated = record['last_updated']
-		self. station_id = record['station_id']
-		self.num_bikes_available = record['num_bikes_available']
-		self.num_docks_available = record['num_docks_available']
-		self.is_installed = record['is_installed']
-		self.is_renting = record['is_renting']
-		self.is_returning = record['is_returning']
-		self.last_reported = record['last_reported']
+		if type(self.record) == tuple:
+			self.last_updated = self.record[0]
+			self.station_id = self.record[1]
+			self.num_bikes_available = self.record[2]
+			self.num_bikes_disabled = self.record[3]
+			self.num_docks_available = self.record[4]
+			self.num_docks_disabled = self.record[5]
+			self.is_installed = self.record[6]
+			self.is_renting = self.record[7]
+			self.is_returning = self.record[8]
+			self.last_reported = self.record[9]
+
+		elif type(self.record) == dict:
+
+			self.last_updated = record['last_updated']
+			self. station_id = record['station_id']
+			self.num_bikes_available = record['num_bikes_available']
+			self.num_docks_available = record['num_docks_available']
+			self.is_installed = record['is_installed']
+			self.is_renting = record['is_renting']
+			self.is_returning = record['is_returning']
+			self.last_reported = record['last_reported']
 
 
-		#optionals
-		self.set_num_bikes_disabled()
-		self.set_num_docks_disabled()
+			#optionals
+			self.set_num_bikes_disabled()
+			self.set_num_docks_disabled()
+
+		else:
+			raise TypeError('Record must be tuple or dict!')
+
+
 
 	def set_num_bikes_disabled(self):
 		try:
---
+++ station_status_cdc.py
@@ -0,0 +1,121 @@
+#station_status_cdc.py
+
+import sqlite3
+from time import time,sleep,strftime
+import requests
+from models.station_status import Station_Status
+
+def get_latest_from_db(db):
+	'''
+	get latest station_status data from database
+	returns list of 
+	'''
+	connection = sqlite3.connect(db)
+	latest_sql = '''SELECT 
+					station_id,
+					last_updated,
+					num_bikes_available,
+					num_bikes_disabled,
+					num_docks_available,
+					num_docks_disabled,
+					is_installed,
+					is_renting,
+					is_returning,
+					last_reported
+					FROM v_station_status'''
+	latest = connection.execute(latest_sql).fetchall()
+	#convert list of tuples to dict with station_id as key
+	latest_dict = {}
+	for row in latest:
+		latest_dict[row[0]] = row[1:]
+	return latest_dict
+
+
+def get_data_api():
+	'''
+	get data from api
+	make sure to return data, last_updated, and ttl
+	do so in a way so comparison is easy to get_latest_from_db
+	'''
+	url = 'https://gbfs.capitalbikeshare.com/gbfs/en/station_status.json'
+	response = requests.get(url)
+	while response.status_code != 200: #if can't access api
+		write_log(f'html response {response.status_code}')
+		sleep(10)
+		response = requests.get(url)
+		#some sort of notice on error?
+
+	return response.json()
+
+def write_log(message):
+	with open('cdc_log.txt','a') as file:
+		file.write(f'{message}\t||\t{(strftime("%c"))}\n')
+
+
+def compare(new_row,old_row,last_updated,out,new_data):
+	'''
+	compare new and old
+	if changed, add data to update and new_data
+	'''
+	old_row = list(old_row) #convert from tuple to list
+	new_row['last_updated'] = last_updated
+	new_row = Station_Status(new_row).to_list()
+	if new_row[2:-1] != old_row[1:-1]: #if changed
+		#out is list to be inserted into db
+		out.append(new_row)
+		#replace old data in db_data dict with new
+		#pop station ID from index 1 for key
+		new_data[new_row.pop(1)] = new_row
+
+
+def load_db(out):
+	'''
+	load new data into db
+	'''
+	with open('test_out','a') as file:
+		for row in out:
+			file.write(str(row))
+
+	print(f'updated {len(out)} rows.')
+
+
+#######################################
+#######################################
+### LOOP IS UPDATING ALL EVERY TIME ###
+#######################################
+#######################################
+
+def station_status_cdc(db):
+	db_data = get_latest_from_db(db)
+	nextFileTstmp = 0
+	while True:
+		out = [] #reset each loop
+		if int(time()) >= nextFileTstmp:
+			new_data = get_data_api()
+			for new_row in new_data['data']['stations']:
+				try:
+					if new_row['last_reported'] != db_data[int(new_row['station_id'])][-1]: #last spot in latest_dict is last_reported
+						compare(new_row,db_data[int(new_row['station_id'])],\
+								new_data['last_updated'],out,db_data)
+				except KeyError: 
+					#key error means not in db_data so insert new record
+					new_row['last_updated'] = new_data['last_updated']
+					new_row = Station_Status(new_row).to_list()
+					out.append(new_row)
+					db_data[new_row.pop(1)] = new_row #add to latest
+			load_db(out)  
+
+			nextFileTstmp = new_data['last_updated'] + new_data['ttl']
+			if nextFileTstmp-time() > 0:
+				print(f'got data. sleepng for {nextFileTstmp-time()} seconds')
+				sleep(nextFileTstmp-time())
+		else:
+			sleep(nextFileTstmp-time())
+
+def main():
+	db = 'bikeshare.db'
+	station_status_cdc(db)
+
+
+if __name__ == '__main__':
+	main()
